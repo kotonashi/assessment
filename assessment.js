0d06e51d7152c1f9995625c9c302bbfa46181224
コトナシ 生きていた <aaa>
2018-08-15 15:06:13 +0900

    css追加

---
+++ assessment.js
@@ -0,0 +1,5 @@
+(function (){
+    'use strict';
+    const userNameInput=document.getElementById('user-name');
+    const assessmentButton = document.getElementById('assessment');
+    const resultDivided=document.getElementBy
